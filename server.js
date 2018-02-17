93595f5cb6b9758333aa8f68d0c5a7efd45b6a73
ShashankDevatha <sasankdevatha11@gmail.com>
2018-02-18 00:09:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-ome.html');
+    res.sendFile(path.join(__dirname,'ui','article-one.html');
 });
 
 app.get('/article-two',function(req,res){
