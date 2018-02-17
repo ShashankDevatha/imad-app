089fabaa8b486df1a7f261c2b7b7fc5d563b8f41
ShashankDevatha <sasankdevatha11@gmail.com>
2018-02-17 23:48:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.sendFile('article one requested is served');
+});
+
+app.get('/article-two',function(req,res){
+    res.sendFile('article two requested is served');
+});
+
+app.get('/article-three',function(req,res){
+    res.sendFile('article three requested is served');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
