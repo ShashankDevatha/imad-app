94224d950acc59c8a5723ebc3aeaacde739b6a71
ShashankDevatha <sasankdevatha11@gmail.com>
2018-02-18 18:04:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title : 'article-one',
+    date: 'Feb 18,2018',
+    heading : 'Article-one',
+    content : ` <p>
+            This is the content for my First Article.Please enjoy reading
+            </p>
+        <p>
+            This is the content for my First Article.Please enjoy reading
+        </p>
+        <p>
+            This is the content for my First Article.Please enjoy reading
+        </p>
+        <p>
+            This is the content for my First Article.Please enjoy reading
+        </p>`
+    
+};
+function templatedata(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date= data.date;
+    var content= data.content;
+    
+var htmlTemp=
+`<html>
+    <head>
+        <title>${title}<title>
+        <meta name="viewport" content="width=device-width , intial-scale=1">
+        <link href="ui/style.css" rel="spreadsheet"
+        </head>
+    <body>
+        
+        <div class="container">
+            <div>
+            <a href="/">click here for home</a>
+        </div>
+        <h1>
+            ${heading}
+        </h1>
+    <div>
+        ${date}
+    </div>
+    <div>
+       ${content}
+    </div>
+    </div>
+    </body>
+</html>`;
+return htmlTemp;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function (req, res) {
+  res.send(temaplatedata(articleOne));
 });
 
 app.get('/article-one',function(req,res){
@@ -14,7 +69,7 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.sendFile(path.join(__dirname,"ui","article-two.html"))
+    res.sendFile(path.join(__dirname,"ui","article-two.html"));
 });
 
 app.get('/article-three',function(req,res){
