d9c4e98778609e48cce0a000c2c346025634abc8
ShashankDevatha <sasankdevatha11@gmail.com>
2018-02-19 06:57:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ alert('Hi!');
 var content = document.getElementById('main-text');
 content;
 element.innerHtml='New Value';
+//Moving The Image
+var move = document.getElementById("img");
+move.onclick = function(){
+   
+ img.style.margin-left = "20px";
+} ;
