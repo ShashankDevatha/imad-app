3e06d6e93398ecd600dee861b4027988292aabea
ShashankDevatha <sasankdevatha11@gmail.com>
2018-02-19 21:19:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,4 @@
-console.log('Loaded!');
-alert('Hi!');
-var content = document.getElementById('main-text');
-//Moving The Image
-var move = document.getElementById('img');
-move.onclick = function(){
-   
- img.style.marginLeft = '100px';
-} ;
+
 var counter=0;
 var button = doucument.getElementById('counter');
 button.onclick = function ()
